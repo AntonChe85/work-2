c722d953470e73b000e2168e34918c769bca9691
Anton Cherkasov <Anton.che85@gmail.com>
2016-02-23 01:50:06 +0200

    content, sidebar ...

--- app/js/slider.js
+++ app/js/slider.js
@@ -5,11 +5,11 @@ $(function() {
       max: 50000,
       values: [ 100, 13000 ],
       slide: function( event, ui ) {
-        $( "#amount-1" ).val( "$" + ui.values[ 0 ] );
-        $( "#amount-2" ).val( "$" + ui.values[ 1 ] );
+        $( "#amount-1" ).val( ui.values[ 0 ] );
+        $( "#amount-2" ).val( ui.values[ 1 ] );
       }
 
     });
-    $( "#amount-1" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) );
-    $( "#amount-2" ).val( "$" + $( "#slider-range" ).slider( "values", 1 ) );
+    $( "#amount-1" ).val( $( "#slider-range" ).slider( "values", 0 ) );
+    $( "#amount-2" ).val( $( "#slider-range" ).slider( "values", 1 ) );
   });
